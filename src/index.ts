ccab10fac0b12785c3d14f2660837a303fadc91d
biubiupiu1 <18827785313@163.com>
2025-09-07 20:49:39 +0800

    feat: return image list from downloadFigmaImages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { downloadFigmaImages } from "./tools/download-figma-images.js";
-export type { DownloadImagesParams } from "./tools/download-figma-images.js";
+export type { DownloadImagesParams, DownloadedImage } from "./tools/download-figma-images.js";
 export { getFigmaData } from "./tools/get-figma-data.js";
 export type { GetFigmaDataParams } from "./tools/get-figma-data.js";
 export { FigmaService } from "./services/figma.js";
